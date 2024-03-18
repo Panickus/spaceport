2cf15813e899b1cd3fd2f9455192e240160d69c7
panickus <felipedg@outlook.es>
2024-03-18 20:33:10 +0100

    Complete Navbar, projects, and Skills components.

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="h-full w-full">
-      <div className="flex flex-col h-[850px] gap-20">
+      <div className="flex flex-col h-[750px] gap-20">
           <Hero />
       </div>
     </main>
   );
-
-}
+};
